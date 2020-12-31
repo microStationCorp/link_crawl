fa1339d844178a90f6475e3723defca55fdd4ffe
sherlock <sujanmondal916@gmail.com>
2020-12-31 10:32:59 +0530

    comment added

--- get_url.py
+++ get_url.py
@@ -4,28 +4,38 @@ from urllib.parse import urlsplit
 
 
 def get_local_urls(url):
+    # declare all variables as set
     local_urls = set()
     foreign_urls = set()
     files_urls = set()
 
+    # to see if the link is broken or not
     try:
         response = requests.get(url, "html.parser")
     except(requests.exceptions.MissingSchema, requests.exceptions.ConnectionError, requests.exceptions.InvalidURL,
            requests.exceptions.InvalidSchema):
+        # if link is broken then return Broken tag True
         return local_urls, foreign_urls, files_urls, True
 
+    # split url in parts
     parts = urlsplit(url)
+    # create base url
     base = "{0.netloc}".format(parts)
     strip_base = base.replace("www.", "")
+    # create base url
     base_url = "{0.scheme}://{0.netloc}".format(parts)
-
+    # create path
     path = url[:url.rfind("/") + 1] if "/" in parts.path else url + "/"
 
+    # get full website
     soup = BeautifulSoup(response.text, "html.parser")
 
+    # loop through all anchors
     for link in soup.find_all("a"):
         anchor = link.attrs["href"] if "href" in link.attrs else ""
 
+        # if the anchor is file or not in this I only consider .pdf and .jpg .
+        # In your logic you may have many more files you can add those as per logic
         if (anchor.endswith(".pdf") or anchor.endswith(".PDF") or anchor.endswith(".jpg")) and anchor.startswith("/"):
             files_urls.add(base_url + anchor)
         elif anchor.endswith(".pdf") or anchor.endswith(".PDF") or anchor.endswith(".jpg"):
@@ -37,8 +47,10 @@ def get_local_urls(url):
         elif not anchor.startswith("http"):
             local_urls.add(path + anchor)
         else:
+            # get all foreign urls
             foreign_urls.add(anchor)
 
+    # return all local foreign file urls and broken tag
     return local_urls, foreign_urls, files_urls, False
 
 
--- main.py
+++ main.py
@@ -2,25 +2,37 @@ from collections import deque
 import json
 import get_url
 
+# main website url
 URL = "http://rjshayari.com/"
 
+# variable which holds all the data
 MAIN_DATA = []
 
+# new urls which have to be fetch
 new_urls = deque([URL])
+# list of urls which are fetched
 processed_urls = set()
 
+# loop through all the new urls
 while len(new_urls):
+    # take one url from from "new_urls" set
     url = new_urls.popleft()
+    # add to "processed_urls"
     processed_urls.add(url)
 
+    # run get_url function and get local, foreign , files url and get broken tag to know if the link is broken
     local, foreign, files, broken = get_url.get_local_urls(url)
 
+    # print processing url
     print("processing : ", url)
 
+    # if the link is not broken
     if not broken:
         for i in local:
+            # if the local urls are already not fetched , then add to "new_urls"
             if i not in new_urls and i not in processed_urls:
                 new_urls.append(i)
+        # append MAIN_DATA variable
         MAIN_DATA.append({
             "processed_url": url,
             "local_urls": list(local),
@@ -33,5 +45,6 @@ while len(new_urls):
             "broken": broken
         })
 
+# after all run make a json file
 with open("links.json", "w") as file:
     json.dump(MAIN_DATA, file, indent=4)
